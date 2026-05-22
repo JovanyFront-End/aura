8f8b0ab9e0c7b4e8d6b0389ada62c0ed78a5ade5
Jovany <JovanyDevStudio@Gmail.com>
2026-05-23 02:54:39 +0300

    publish sec

--- Aura/vite.config.js
+++ Aura/vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/aura/',
-})
-
-
-export default defineConfig({
-    plugins: [
-    tailwindcss(),
-    ],
 })
