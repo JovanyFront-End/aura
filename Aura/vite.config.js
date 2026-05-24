1926a1474a79f28e35918255f09e328e2cfeeb26
Jovany <JovanyDevStudio@Gmail.com>
2026-05-24 16:29:55 +0300

    vte cinfig

--- Aura/vite.config.js
+++ Aura/vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/aura/',
-
     plugins: [
         tailwindcss(),
     ],
